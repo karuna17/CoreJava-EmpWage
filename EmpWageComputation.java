2ab4e9c953f884949e24a9f19953ac3dddbb7f8f
karuna <karunakakade8@gmail.com>
2021-05-13 17:19:30 +0530

    Added: Calculate Monthaly Employee wage and no of hrs

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,12 +4,15 @@ public class EmpWageComputation {
 	static final int partTime=2;
 	static int ratePerHr=20;
 	static int TotalDays=20;
-
+	
 	public static void main(String[] args) {
+		
 		int empHrs = 0;
 		int salary = 0;
 		int TotalSalary=0;
-		for(int i=1;i<=TotalDays;i++)
+		int day=0;
+		int noOfHrs=0;
+		while(day<TotalDays && noOfHrs<=100)
 		{		
 			int empCheck=(int) (Math.floor(Math.random()*10)%3);
 			switch(empCheck)
@@ -26,7 +29,10 @@ public class EmpWageComputation {
 				empHrs=0;
 			}
 		    TotalSalary=TotalSalary+salary;
+		    noOfHrs=noOfHrs+empHrs;
+		    day++;
 		}
-		System.out.println("Employee Wage: "+TotalSalary);
+		System.out.println("Total No Of Hrs: "+noOfHrs);
+		System.out.println("Total Employee Wage: "+TotalSalary);
 	}
 }
