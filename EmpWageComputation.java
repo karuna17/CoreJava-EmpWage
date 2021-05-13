ee2b787d3d876d9a5ab85543258e55978b0860a0
karuna <karunakakade8@gmail.com>
2021-05-13 15:50:17 +0530

    Added: Calculate Daily Employee wage for part time and full time employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,23 +3,28 @@ public class EmpWageComputation {
 
 	public static void main(String[] args) {
 		int fullTime=1;
+		int partTime=2;
 		int ratePerHr=20;
 		int salary;
-		int empCheck=(int) (Math.floor(Math.random()*10)%2);
+		int empCheck=(int) (Math.floor(Math.random()*10)%3);
 		if(empCheck==fullTime)
 		{
-			System.out.println("Emloyee is present");
+			System.out.println("Emloyee is Fulltime");
 			int empHrs=8;
 			salary=empHrs*ratePerHr;
 			System.out.println("Employee Wage: "+salary);
 		}
-		else
+		else if(empCheck==partTime)
 		{
-			System.out.println("Employee is absent");
-			int empHrs=0;
+			System.out.println("Employee is PartTime");
+			int empHrs=4;
 			salary=empHrs*ratePerHr;
 			System.out.println("Employee Wage: "+salary);
 		}	
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 
 	}
 
